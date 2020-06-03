16dcf9bdaec174902d5eb543a230e01cda62130e
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-06-03 17:09:26 +0100

    Adds NavBar component and empty About component. Being rendered

---
+++ src/shared/components/Navigation/NavBar.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        
+        <Navbar.Brand href="#home">Emily Spencer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link href="about">About</Nav.Link>
+            <Nav.Link href="portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="cv">CV</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+
+
+export default NavBar;
