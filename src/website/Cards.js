8c56f4716f63d6f10c946d1619142cf9344b5338
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-07-09 11:38:38 +0100

    Adds Surge links to relevant projects

--- src/website/Cards.js
+++ src/website/Cards.js
@@ -16,32 +16,27 @@ const Cards = () => {
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="FinBot" >
                   <Typography color="textSecondary" gutterBottom>FinBot</Typography>
-                  <Typography variant="h5">
-                  </Typography>
+                  <Typography variant="h5"></Typography>
                   <Typography color="textSecondary"></Typography>
                   <Typography variant="body2">A chatbot</Typography>
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Global Reads">
                   <Typography color="textSecondary" gutterBottom>Global Reads</Typography>
-                  <Typography variant="h5">
-                  </Typography>
+                  <Typography variant="h5"></Typography>
                   <Typography color="textSecondary"></Typography>
                   <Typography variant="body2">A Rails application</Typography>
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Chitter">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Chitter</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography variant="body2">A sinatra/postgresql application</Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Rock, Paper, Scissors">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Rock Paper Scissors</Typography>
-                     <Typography variant="h5">
-        
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A sinatra-based application</Typography>
                      </CardContent> 
@@ -49,8 +44,7 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="Thermostat">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Thermostat</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">An application built with JavaScript and jQuery. Test-driven with Jasmine</Typography>
                      </CardContent> 
@@ -58,26 +52,25 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="Pristine Pics">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Pristine Pics</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">Pristine Pics</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://guiltless-clouds.surge.sh"></a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="TimesTables">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Times Tables</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">Times Tables</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://useful-cushion.surge.sh">Times Tables</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="StudentRegister">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Student Register</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A Pern-stack Application</Typography>
                      </CardContent> 
@@ -85,8 +78,7 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="FindPlaces">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Find Places</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A Mern-stack Application</Typography>
                      </CardContent> 
@@ -94,8 +86,7 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="FindAMovie">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Find a Movie</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">An Express/Node application</Typography>
                      </CardContent> 
@@ -103,27 +94,25 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="Presents Planner">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Presents Planner</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A React application. Test-driven with Jasmine and Enzyme</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://pale-plastic.surge.sh">Presents Planner</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Bucket List">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Bucket List</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A React application. Test-driven with Jasmine and Enzyme</Typography>
-                     <Typography variant="body2">Try it for yourself: <a href="wealthy-cover.surge.sh">Bucket List</a></Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://wealthy-cover.surge.sh">Bucket List</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Bookmarks">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Bookmarks</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A Sinatra based application. Built with Ruby, Sinatra and PostgreSQL</Typography>
                      </CardContent> 
@@ -131,8 +120,7 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="The Book Lounge">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>The Book Lounge</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A Sinatra based application. Built with Ruby, Sinatra and PostgreSQL</Typography>
                      </CardContent> 
@@ -140,8 +128,7 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="Bookify">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Bookify</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A Mern-stack Application</Typography>
                      </CardContent> 
@@ -149,37 +136,55 @@ const Cards = () => {
               <Grid item component={Card} xs={12} md={3} className="Potter World">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Potter World</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A React Application. Uses the Unofficial Harry Potter API</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://........surge.sh/">Potter World</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Guess the shade">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Guess the Shade</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A game built with vanilla JavaScript</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://profuse-swing.surge.sh/">Guess the Shade</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Guardian News">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Guardian News</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A React application which calls on the Guardian API</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://sable-notebook.surge.sh/">Guardian News</a></Typography>
                      </CardContent> 
               </Grid>
               <Grid item component={Card} xs={12} md={3} className="Guardian News">
                  <CardContent>
                      <Typography color="textSecondary" gutterBottom>Guardian News</Typography>
-                     <Typography variant="h5">
-                            </Typography>
+                     <Typography variant="h5"></Typography>
                      <Typography color="textSecondary"></Typography>
                      <Typography variant="body2">A single page application which calls on the Guardian API. Build using vanilla JavaScript and no testing library</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://..........surge.sh">Guardian News</a></Typography>
+                     </CardContent> 
+              </Grid>
+              <Grid item component={Card} xs={12} md={3} className="Alohomora">
+                 <CardContent>
+                     <Typography color="textSecondary" gutterBottom>Alohomora</Typography>
+                     <Typography variant="h5"></Typography>
+                     <Typography color="textSecondary"></Typography>
+                     <Typography variant="body2">A Rails application that uses websockets. Simulates a real-time chat application. For fans of Harry Potter</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href=".......">Alohomora</a></Typography>
+                     </CardContent> 
+              </Grid>
+              <Grid item component={Card} xs={12} md={3} className="Meditate">
+                 <CardContent>
+                     <Typography color="textSecondary" gutterBottom>Meditate</Typography>
+                     <Typography variant="h5"></Typography>
+                     <Typography color="textSecondary"></Typography>
+                     <Typography variant="body2">A React application for those who want to meditate.</Typography>
+                     <Typography variant="body2">Try it for yourself: <a href="http://..........surge.sh">Meditate</a></Typography>
                      </CardContent> 
               </Grid>
             </Grid>
