3386e8513b12e1f4b34ad95b4665b68acd4407c8
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-10-19 01:06:03 +0100

    Adds some travel pictures to carousel

---
+++ src/website/CarouselTravel.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Carousel from 'nuka-carousel';
+
+import './CarouselTravel.css';
+import nice from './images/cards/nice.png';
+import brecht from './images/cards/brecht.png';
+import chopin from './images/cards/chopin.png';
+import mozart from './images/cards/mozart.png';
+import alpine from './images/cards/alpine.png';
+import disney from './images/cards/disney1.png';
+import amsterdam from './images/cards/amsterdam.png';
+import berlin from './images/cards/berlin.png';
+import neuschwanstein3 from './images/cards/neuschwanstein.png';
+import eiffel from './images/cards/eiffel.png';
+
+
+export default class extends React.Component {
+  render() {
+    return (
+      <Carousel slideWidth="360px">
+           <img className="mozart" src={mozart}/>
+           <img className="neuschwanstein" src={neuschwanstein3}/>
+        <img className="brecht" src={brecht}/>
+        <img className="berlin" src={berlin} />
+        <img className="eiffel" src={eiffel} />
+        <br />
+        <img className="chopin" src={chopin}/>
+        <img className="brecht" src={brecht} />
+        <img className="amsterdam" src={amsterdam}/>
+        <img className="nice" src={nice}/>
+        <img className="alpine" src={alpine}/>
+        <br />
+        <img className="disney" src={disney}/>
+       
+        
+       
+      </Carousel>
+    );
+  }
+}
